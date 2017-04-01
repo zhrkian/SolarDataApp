25376a392b06860f2722659a0a5ee66935166005
Fedor Troitsky <fedor.troitsky@gmail.com>
2017-04-01 14:20:42 +0300

    update icon buttons layout

--- app/components/IconButton/IconButton.js
+++ app/components/IconButton/IconButton.js
@@ -5,9 +5,11 @@ import * as Icons from '../Icons/Icons'
 const IconButton = props => {
   const Icon = Icons[props.icon]
   return (
-    <div className={s.button}>
-      <Icon />
-      <span className={s.label}>{props.label}</span>
+    <div className={s.container}>
+      <div className={s.icon}>
+        <Icon />
+      </div>
+      <div className={s.label}>{props.label}</div>
     </div>
   )
 }
