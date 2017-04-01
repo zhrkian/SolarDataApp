3a8d889fd1b9a6d7a75a0417d8fb1d998d1a3508
Fedor Troitsky <fedor.troitsky@gmail.com>
2017-04-01 14:02:25 +0300

    fix calc icon

--- app/components/Icons/Icons.js
+++ app/components/Icons/Icons.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export const Remove = () =>
   <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46">
-    <title>99E5E27F-403A-4E41-9BAD-1F119ACA7A88</title>
     <g stroke="#FF1453" strokeWidth="2" fill="none" fillRule="evenodd" strokeLinecap="round">
       <path d="M34.795 10.41L9.893 35.31M35.713 36.006L10.812 11.105"/>
     </g>
@@ -10,7 +9,6 @@ export const Remove = () =>
 
 export const RemoveOne = () =>
   <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46">
-    <title>0D71BEA0-9130-42BB-8642-5BDB96E351B4</title>
     <g fill="none" fillRule="evenodd">
       <g stroke="#FF1453" strokeWidth="2" strokeLinecap="round">
         <path d="M34.795 10.41L9.893 35.31M35.713 36.006L10.812 11.105"/>
@@ -21,7 +19,6 @@ export const RemoveOne = () =>
 
 export const Contour = () =>
   <svg width="46" height="46" viewBox="0 0 46 46" xmlns="http://www.w3.org/2000/svg">
-    <title>FF6048EB-6EA4-4B71-A954-FF11971A8A10</title>
     <g fill="none" fillRule="evenodd">
       <path stroke="#FE0" strokeWidth="2" d="M21.806 38.895L7 13.145 38 6v21.33z"/>
       <circle fill="#FFF" cx="7" cy="13" r="3"/><circle fill="#FFF" cx="38" cy="6" r="3"/>
@@ -31,7 +28,6 @@ export const Contour = () =>
 
 export const Area = () =>
   <svg width="46" height="46" viewBox="0 0 46 46" xmlns="http://www.w3.org/2000/svg">
-    <title>13A34472-19AE-46BC-991D-37DE7EDD73AA</title>
     <defs>
       <path id="a" d="M14.806 32.895L0 7.145 31 0v21.33z"/>
     </defs>
@@ -45,21 +41,7 @@ export const Area = () =>
   </svg>
 
 export const Calc = () =>
-  <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46">
-    <path fill="#50E3C2" fillRule="evenodd" d="M5 8c0-1.7 1.3-3 3-3h30c1.7 0 3 1.3 3 3v30c0 1.7-1.3 3-3 3H8c-1.7 0-3-1.3-3-3V8zm3-1h30c.5 0 1 .5 1 1v30c0 .5-.5 1-1 1H8c-.5 0-1-.5-1-1V8c0-.5.5-1 1-1z"/>
-    <path fill="none" stroke="#50E3C2" strokeWidth="2" d="M23 6.5V40m17-17H6.3"/>
-    <path fill="#50E3C2" fillRule="evenodd" d="M14.6 14H17v1.2h-2.4v3h-1.2v-3h-2.6V14h2.6v-2.7h1.2V14zm.3 17.7l1.6 1.8-.8.8-2-2-2 2.2-.8-.8 2-2-1.8-2 .8-.7 1.8 1.8 2-2 .8 1-2 2zM34 15h-6v-1h6v1zm0 16h-5v-1h5v1zm0 3h-5v-1h5v1z"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 46" width="46" height="46">
+    <path fill="#50E3C2" fill-rule="evenodd" d="M24 22V7h-2v15H7v2h15v15h2V24h15v-2H24zM5 8c0-1.7 1.3-3 3-3h30c1.7 0 3 1.3 3 3v30c0 1.7-1.3 3-3 3H8c-1.7 0-3-1.3-3-3V8zm3-1h30c.5 0 1 .5 1 1v30c0 .5-.5 1-1 1H8c-.5 0-1-.5-1-1V8c0-.5.5-1 1-1z"/>
+    <path fill="#50E3C2" fill-rule="evenodd" d="M14 14h-3v1h3v3h1v-3h3v-1h-3v-3h-1v3zm.5 16.8L11.7 28l-.7.7 2.8 2.8-2.8 3 .7.6 2.8-2.6 3 3 .6-.8-2.6-3 3-2.7-.8-.8-3 3zM28 14h7v1h-7v-1zm0 16h7v1h-7v-1zm0 2h7v1h-7v-1z"/>
   </svg>
-  //<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="46" height="46" viewBox="0 0 46 46">
-  //  <title>5F3849FB-F86A-42DE-A066-3A58343952C6</title>
-  //  <defs>
-  //    <rect id="a" x="5" y="5" width="36" height="36" rx="3"/>
-  //    <mask id="b" x="0" y="0" width="36" height="36" fill="#fff">
-  //      <use xlinkHref="#a"/>
-  //    </mask></defs>
-  //  <g fill="none" fillRule="evenodd">
-  //    <use stroke="#50E3C2" mask="url(#b)" strokeWidth="4" xlinkHref="#a"/>
-  //    <path d="M22 6.5v33.515M38.016 23H6.266" stroke="#50E3C2" strokeWidth="2" strokeLinecap="square"/>
-  //    <path fill="#50E3C2" d="M14.568 14.042h2.577v1.111h-2.577v2.92h-1.18v-2.92H10.81v-1.11h2.577v-2.698h1.181zM14.867 31.655l1.822 1.822-.786.786-1.822-1.822-2.065 2.064-.835-.835 2.065-2.064-1.822-1.823.785-.785 1.823 1.822 1.907-1.907.835.835zM34 15h-6v-1h6zM34 31h-5v-1h5v1zm0 3h-5v-1h5v1z"/>
-  //  </g>
-  //</svg>
