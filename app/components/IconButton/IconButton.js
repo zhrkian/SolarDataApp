0f311f362449696da2d1a8e699b8f286699f5dd4
Fedor Troitsky <fedor.troitsky@gmail.com>
2017-04-01 15:05:58 +0300

    update icon buttons

--- app/components/IconButton/IconButton.js
+++ app/components/IconButton/IconButton.js
@@ -1,16 +1,19 @@
 import s from './IconButton.css'
 import React from 'react'
+import IButton from 'material-ui/IconButton';
 import * as Icons from '../Icons/Icons'
 
 const IconButton = props => {
   const Icon = Icons[props.icon]
   return (
-    <div className={s.container}>
+    <IButton className={s.container} tooltip={props.label.length > 12 ? props.label : null }>
       <div className={s.icon}>
         <Icon />
       </div>
-      <div className={s.label}>{props.label}</div>
-    </div>
+      <div className={s.label}>
+        <div className={s.text}>{props.label}</div>
+      </div>
+    </IButton>
   )
 }
 
