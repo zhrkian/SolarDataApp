76c45928f8cc60d90cde8b0b5e18e3928da6dfef
R.Zhygalkin <r.zhygalkin@gmail.com>
2017-04-01 13:57:31 +0300

    Ipdate Calc icon

--- app/components/Icons/Icons.js
+++ app/components/Icons/Icons.js
@@ -45,16 +45,21 @@ export const Area = () =>
   </svg>
 
 export const Calc = () =>
-  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="46" height="46" viewBox="0 0 46 46">
-    <title>5F3849FB-F86A-42DE-A066-3A58343952C6</title>
-    <defs>
-      <rect id="a" x="5" y="5" width="36" height="36" rx="3"/>
-      <mask id="b" x="0" y="0" width="36" height="36" fill="#fff">
-        <use xlinkHref="#a"/>
-      </mask></defs>
-    <g fill="none" fillRule="evenodd">
-      <use stroke="#50E3C2" mask="url(#b)" strokeWidth="4" xlinkHref="#a"/>
-      <path d="M22 6.5v33.515M38.016 23H6.266" stroke="#50E3C2" strokeWidth="2" strokeLinecap="square"/>
-      <path fill="#50E3C2" d="M14.568 14.042h2.577v1.111h-2.577v2.92h-1.18v-2.92H10.81v-1.11h2.577v-2.698h1.181zM14.867 31.655l1.822 1.822-.786.786-1.822-1.822-2.065 2.064-.835-.835 2.065-2.064-1.822-1.823.785-.785 1.823 1.822 1.907-1.907.835.835zM34 15h-6v-1h6zM34 31h-5v-1h5v1zm0 3h-5v-1h5v1z"/>
-    </g>
+  <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46">
+    <path fill="#50E3C2" fillRule="evenodd" d="M5 8c0-1.7 1.3-3 3-3h30c1.7 0 3 1.3 3 3v30c0 1.7-1.3 3-3 3H8c-1.7 0-3-1.3-3-3V8zm3-1h30c.5 0 1 .5 1 1v30c0 .5-.5 1-1 1H8c-.5 0-1-.5-1-1V8c0-.5.5-1 1-1z"/>
+    <path fill="none" stroke="#50E3C2" strokeWidth="2" d="M23 6.5V40m17-17H6.3"/>
+    <path fill="#50E3C2" fillRule="evenodd" d="M14.6 14H17v1.2h-2.4v3h-1.2v-3h-2.6V14h2.6v-2.7h1.2V14zm.3 17.7l1.6 1.8-.8.8-2-2-2 2.2-.8-.8 2-2-1.8-2 .8-.7 1.8 1.8 2-2 .8 1-2 2zM34 15h-6v-1h6v1zm0 16h-5v-1h5v1zm0 3h-5v-1h5v1z"/>
   </svg>
+  //<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="46" height="46" viewBox="0 0 46 46">
+  //  <title>5F3849FB-F86A-42DE-A066-3A58343952C6</title>
+  //  <defs>
+  //    <rect id="a" x="5" y="5" width="36" height="36" rx="3"/>
+  //    <mask id="b" x="0" y="0" width="36" height="36" fill="#fff">
+  //      <use xlinkHref="#a"/>
+  //    </mask></defs>
+  //  <g fill="none" fillRule="evenodd">
+  //    <use stroke="#50E3C2" mask="url(#b)" strokeWidth="4" xlinkHref="#a"/>
+  //    <path d="M22 6.5v33.515M38.016 23H6.266" stroke="#50E3C2" strokeWidth="2" strokeLinecap="square"/>
+  //    <path fill="#50E3C2" d="M14.568 14.042h2.577v1.111h-2.577v2.92h-1.18v-2.92H10.81v-1.11h2.577v-2.698h1.181zM14.867 31.655l1.822 1.822-.786.786-1.822-1.822-2.065 2.064-.835-.835 2.065-2.064-1.822-1.823.785-.785 1.823 1.822 1.907-1.907.835.835zM34 15h-6v-1h6zM34 31h-5v-1h5v1zm0 3h-5v-1h5v1z"/>
+  //  </g>
+  //</svg>
